5cf81af4233dc08df9ea99ed321f9bbe0bf3b8cd
nico <n@n.xom>
2020-05-21 09:45:56 -0400

    just run npm test

--- code-challenge/src/game.js
+++ code-challenge/src/game.js
@@ -1,1 +1,44 @@
-console.log('no game!');
+function game(grid, cols, rows) {
+	// create brand new deep-copy grid
+	let next = grid.map(inner => inner.slice());
+
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			let state = grid[i][j];
+			// live neighbors counting
+			let neighbors = countNeighbors(grid, i, j, cols, rows);
+
+			if (state === 0 && neighbors === 3) {
+				next[i][j] = 1;
+			} else if (state === 1 && (neighbors < 2 || neighbors > 3)) {
+				next[i][j] = 0;
+			} else {
+				next[i][j] = state;
+			}
+		}
+	}
+	return next;
+}
+
+function countNeighbors(grid, x, y, cols, rows) {
+	let sum = 0;
+	for (let i = -1; i < 2; i++) {
+		for (let j = -1; j < 2; j++) {
+			let col = (x + i + cols) % cols;
+			let row = (y + j + rows) % rows;
+			sum += grid[col][row];
+		}
+	}
+	sum -= grid[x][y];
+	return sum;
+	// sum += grid[i-1][j-1]
+	// sum += grid[i][j-1]
+	// sum += grid[i+1][j-1]
+	// sum += grid[i-1][j]
+	// sum += grid[i+1][j]
+	// sum += grid[i-1][j+1]
+	// sum += grid[i][j+1]
+	// sum += grid[i+1][j+1]
+}
+
+export default game
--- code-challenge/src/game.test.js
+++ code-challenge/src/game.test.js
@@ -1,1 +1,52 @@
-console.error('no tests!');
+import game from './game';
+
+describe('Still life', function () {
+	test('Block', () => {
+		const grid = [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]];
+		let cols = 4;
+		let rows = 4;
+		expect(game(grid, cols, rows)).toStrictEqual(grid);
+	});
+	test('Loaf', () => {
+		const grid = [[0,0,0,0,0,0],[0,0,1,1,0,0],[0,1,0,0,1,0],[0,0,1,0,1,0],[0,0,0,1,0,0],[0,0,0,0,0,0]];
+		let cols = 6;
+		let rows = 6;
+		expect(game(grid, cols, rows)).toStrictEqual(grid);
+	});
+	test('Bee-hive', () => {
+		const grid = [[0,0,0,0,0],[0,0,1,0,0],[0,1,0,1,0],[0,1,0,1,0],[0,0,1,0,0],[0,0,0,0,0]];
+		let cols = 6;
+		let rows = 5;
+		expect(game(grid, cols, rows)).toStrictEqual(grid);
+	});
+	test('Loaf', () => {
+		const grid = [[0,0,0,0,0,0],[0,0,1,0,0,0],[0,1,0,1,0,0],[0,1,0,0,1,0],[0,0,1,1,0,0],[0,0,0,0,0,0]];
+		let cols = 6;
+		let rows = 6;
+		expect(game(grid, cols, rows)).toStrictEqual(grid);
+	});
+});
+
+describe('Oscillators', function () {
+	test('Blinker', () => {
+		const grid =  [[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]];
+		const grid2 = [[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]];
+		let cols = 5;
+		let rows = 5;
+		expect(game(grid, cols, rows)).toStrictEqual(grid2);
+	});
+	test('Toad', () => {
+		const grid =  [[0,0,0,0,0,0],[0,0,0,1,0,0],[0,0,1,1,0,0],[0,0,1,1,0,0],[0,0,1,0,0,0],[0,0,0,0,0,0]];
+		const grid2 = [[0,0,0,0,0,0],[0,0,1,1,0,0],[0,0,0,0,1,0],[0,1,0,0,0,0],[0,0,1,1,0,0],[0,0,0,0,0,0]];
+		let cols = 6;
+		let rows = 6;
+		expect(game(grid, cols, rows)).toStrictEqual(grid2);
+	});
+	test('Beacon', () => {
+		const grid =  [[0,0,0,0,0,0],[0,1,1,0,0,0],[0,1,1,0,0,0],[0,0,0,1,1,0],[0,0,0,1,1,0],[0,0,0,0,0,0]];
+		const grid2 = [[0,0,0,0,0,0],[0,1,1,0,0,0],[0,1,0,0,0,0],[0,0,0,0,1,0],[0,0,0,1,1,0],[0,0,0,0,0,0]];
+		let cols = 6;
+		let rows = 6;
+		expect(game(grid, cols, rows)).toStrictEqual(grid2);
+	});
+});
